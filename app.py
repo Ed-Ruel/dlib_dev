c3b8354fd571611ba0270cf8e7f66dc7e11ffc8d
Ed-Ruel <71631146+Ed-Ruel@users.noreply.github.com>
2023-11-15 14:46:03 +0800

    Update app.py

--- app.py
+++ app.py
@@ -36,10 +36,8 @@ class VideoProcessor:
         for (i, rect) in enumerate(rects):
         	s = predictor(gray, rect)
         	s = face_utils.shape_to_np(s)
-            
             if i==0:
                 print("Total number of face landmarks detected ",len(s_.parts()))
-                    
                 drawPoints(img, s_, 0, 16)           # Jaw line
                 drawPoints(img, s_, 17, 21)          # Left eyebrow
                 drawPoints(img, s_, 22, 26)          # Right eyebrow
